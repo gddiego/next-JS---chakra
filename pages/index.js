352d021e98e77edba3b686a0275962721b8d234d
Diego mendes <gdmendes55@gmail.com>
2020-11-11 23:35:25 -0400

    favicon

--- pages/index.js
+++ pages/index.js
@@ -6,24 +6,23 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>E-coletando</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-96x96.png" />
       </Head>
       <div></div>
       <form>
+        <h3>E-coletando Sistema de Coleta Comunitario</h3>
 
-<h3>E-coletando Sistema de Coleta Comunitario</h3>
+        <label for="username"><h3>Usuario</h3></label>
+        <input type="text" id="username" name="username" placeholder="Entre com seu usuario.." autocomplete="off" required />
 
-<label for="username"><h3>Usuario</h3></label>
-<input type="text" id="username" name="username" placeholder="Entre com seu usuario.." autocomplete="off" required />
+        <label for="password"><h3>Senha</h3></label>
+        <input type="password" id="password" name="password" placeholder="Entre com sua senha.." autocomplete="off" required />
 
-<label for="password"><h3>Senha</h3></label>
-<input type="password" id="password" name="password" placeholder="Entre com sua senha.." autocomplete="off" required />
+        <a class="forgot" href="#">Recuperar Senha?</a>
+        <a class="register" href="#">Registrar</a>
 
-<a class="forgot" href="#">Recuperar Senha?</a>
-<a class="register" href="#">Registrar</a>
-
-<input type="submit" name="submit" value="Entrar" />
-</form>
-</div>
+        <input type="submit" name="submit" value="Entrar" />
+      </form>
+    </div>
   )
 }
